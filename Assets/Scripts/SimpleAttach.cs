c4e63bce6896e64a08261425dca7892e3651c6e4
Quynh Nguyen <39203622+quynhn1404@users.noreply.github.com>
2021-04-25 17:18:42 -0400

    add floor and borders
    add texture to floors and borders and a door

---
+++ Assets/Scripts/SimpleAttach.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+using Valve.VR.InteractionSystem;
+public class SimpleAttach : MonoBehaviour
+{
+    private Interactable interactable;
+    // Start is called before the first frame update
+    void Start()
+    {
+        interactable = GetComponent<Interactable>();
+    }
+
+    private void OnHandHoverBegin(Hand hand)
+    {
+        hand.ShowGrabHint();
+    }
+    private void OnHandHoverEnd(Hand hand)
+    {
+        hand.HideGrabHint();
+    }
+    private void HandHoverUpdate(Hand hand)
+    {
+        GrabTypes grab = hand.GetGrabStarting();
+        bool isGrabEnding = hand.IsGrabEnding(gameObject);
+        // grabbing object
+        if(interactable.attachedToHand == null && grab != GrabTypes.None)
+        {
+            hand.AttachObject(gameObject, grab);
+            hand.HoverLock(interactable);
+            hand.HideGrabHint();
+        }
+        // release
+        else if(isGrabEnding)
+        {
+            hand.DetachObject(gameObject);
+            hand.HoverUnlock(interactable);
+        }
+    }
+}
